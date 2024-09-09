50365fb894588e92b983a89e2ef2deb8c4665ea9
SebySurfer <sebysurfer@gmail.com>
2024-09-09 12:41:53 -0600

    Base

--- main.cpp
+++ main.cpp
@@ -1,2 +1,62 @@
 
 #include "main.h"
+#include<iostream>
+
+using namespace std;
+
+void intercambio(float &x, float &y){
+    float aux;
+    aux = x;
+    x = y;
+    y = aux;
+}
+
+void quickSort(float a[], int primero, int ultimo) {
+    int i, j, central;
+    float pivote;
+
+    central = (primero + ultimo) / 2;
+    pivote = a[central];
+    i = primero;
+    j = ultimo;
+
+    do {
+        while (a[i] < pivote) {
+            i++;
+        }
+        while (a[j] > pivote) {
+            j--;
+        }
+
+        if (i <= j) {
+            intercambio(a[i], a[j]);
+            i++;
+            j--;
+        }
+    } while(i <= j);
+
+    if(primero < j) {
+        quickSort(a, primero, j);
+    }
+    if(i < ultimo){
+        quickSort(a, i, ultimo);
+    }
+
+}
+
+
+int main(){
+
+    float arreglo[] = {3, 5, 7, 3, 2, 5, 9, 7, 1};
+    quickSort(arreglo, 0, 8);
+
+    cout << "Arreglo ordenado ascendente: " << endl;
+
+    for(int i = 0; i < 9; i++){
+        cout << arreglo[i];
+    }
+
+
+
+    return 0;
+}
